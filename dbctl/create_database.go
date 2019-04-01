0ab0c5ba7ad9e933e1e3b078e1a919fd8bced7ff
zf29 <zf29@duke.edu>
2019-03-31 21:58:38 -0400

    database

--- dbctl/create_database.go
+++ dbctl/create_database.go
@@ -1,4 +1,4 @@
-package dbctl
+package main
 
 import(
 	"database/sql"
@@ -29,7 +29,7 @@ const(
 )
 
 func Create_table(db *sql.DB) {
-
+	
 
 	//drop table if exists
 	_,err := db.Exec("DROP TABLE IF EXISTS order_info cascade;")
@@ -46,30 +46,30 @@ func Create_table(db *sql.DB) {
 	activity += "CREATE TABLE activity_info("
 	activity += "activity_id serial primary key,"
 	activity += "order_id int,"
-	activity += "price DECIMAL(6,2),"
+	activity += "price DECIMAL(32,2),"
 	activity += "time timestamptz not null default now());"
 
 	_,err = db.Exec(activity)
 
-	//create table order_info
+	//create table order_info 
 	order := ""
 	order += "CREATE TABLE order_info("
 	order += "order_id int primary key,"
 	order += "account_id int,"
 	order += "open int,"
 	order += "type int,"
-	order += "amount DECIMAL(6,2),"
-	order += "limit_price DECIMAL(6,2));"
+	order += "amount DECIMAL(32,2),"
+	order += "limit_price DECIMAL(32,2));"
 	_,err = db.Exec(order)
-
-	//create table account_info
+	
+	//create table account_info 
 	account := ""
 	account += "CREATE TABLE account_info("
 	account += "account_id text primary key,"
-	account += "balance DECIMAL(6,2));"
+	account += "balance DECIMAL(32,2));"
 	_,err = db.Exec(account)
 
-	//create table symbol_info
+	//create table symbol_info 
 	symbol := ""
 	symbol += "CREATE TABLE symbol_info("
 	symbol += "symbol_id text primary key);"
@@ -103,7 +103,7 @@ func CheckCount(rows *sql.Rows) (count int) {
  	for rows.Next() {
     	err:= rows.Scan(&count)
     	CheckErr(err)
-    }
+    }   
     return count
 }
 
@@ -115,7 +115,7 @@ func CheckErr(err error) {
 
 func Verify_symbol(db *sql.DB, symbol_id string) (int,error){
 	check_s := ""
-	check_s +="select symbol_id from symbol_info where symbol_id='"
+	check_s +="select count(symbol_id) from symbol_info where symbol_id='"
 	check_s += symbol_id
 	check_s += "'"
 	fmt.Println(check_s)
@@ -125,13 +125,14 @@ func Verify_symbol(db *sql.DB, symbol_id string) (int,error){
 	if(count!=0){
 		return ERROR,err
 	}
+
 	defer rows.Close()
 	return INSERT,err
 }
 
 func Verify_account(db *sql.DB, account_id string) (int, error) {
 	check_a := ""
-	check_a +="select account_id from account_info where account_id='"
+	check_a +="select count(account_id) from account_info where account_id='"
 	check_a +=account_id
 	check_a +="'; "
 	fmt.Println(check_a)
@@ -149,7 +150,7 @@ func Verify_account(db *sql.DB, account_id string) (int, error) {
 func Verify_symbol_account(db *sql.DB, symbol_id string, account_id string, num string) (int, error) {
 	//check if account_id exists,if not, return 0, error
 	check_a := ""
-	check_a +="select account_id from account_info where account_id='"
+	check_a +="select count(account_id) from account_info where account_id='"
 	check_a +=account_id
 	check_a +="'; "
 	fmt.Println(check_a)
@@ -157,13 +158,13 @@ func Verify_symbol_account(db *sql.DB, symbol_id string, account_id string, num 
 	rows,err := db.Query(check_a)
 
 	count := CheckCount(rows)
-	if(count != 0){
+	if(count == 0){
 		return ERROR,err
 	}
 
 	//account_id exists, check if it is in symbol_account, if exists, update and return 1, if not, return 2
 	check_sym := ""
-	check_sym +="select account_id from account_to_symbol where account_id='"
+	check_sym +="select count(account_id) from account_to_symbol where account_id='"
 	check_sym +=account_id
 	check_sym +="'; "
 
@@ -185,7 +186,7 @@ func Insert_accout_info(db *sql.DB, account_id string, balance string) error{
 	insert += account_id
 	insert += "', '"
 	insert += balance
-	insert += "'); "
+	insert += "'); " 
 	fmt.Println(insert)
 	_,err := db.Exec(insert)
 	return err
@@ -211,7 +212,7 @@ func Insert_activity_info(db *sql.DB) {
 }
 
 func Insert_order_info(db *sql.DB) {
-
+	
 }
 
 func Insert_symbol_info(db *sql.DB,sym string) error{
@@ -243,19 +244,19 @@ func Connect_database() (db *sql.DB, errstr error){
 	if err != nil{
 		fmt.Println("\nopen mysql error ", err)
 		return db, err
-	}
-
+	}	
+	
 
 	return db,nil
 }
 
 func main() {
 	//connect to the database
-
+	
 	db,_ := Connect_database()
 	fmt.Println("successfully connected!")
 	Create_table(db)
-	db.Exec("insert into account_info values ('123','1234.0');")
+
 	p,_ := Verify_account(db,"12345")
 	fmt.Println(p)
 	p,_ = Verify_account(db,"123")
@@ -265,6 +266,15 @@ func main() {
 	p,_ = Verify_symbol_account(db,"abcd","123","1234.0")
 	fmt.Println(p)
 	Insert_accout_info(db,"12345","1000")
+	Insert_accout_info(db,"123","321")
 	Insert_symbol_info(db,"abcd")
 	Insert_account_to_symbol(db, "abcd", "123", "1234.0")
+	p,_ = Verify_account(db,"12345")
+	fmt.Println(p)
+	p,_ = Verify_account(db,"123")
+	fmt.Println(p)
+	p,_ = Verify_symbol(db,"abcd")
+	fmt.Println(p)
+	p,_ = Verify_symbol_account(db,"abcd","123","1234.0")
+	fmt.Println(p)
 }
